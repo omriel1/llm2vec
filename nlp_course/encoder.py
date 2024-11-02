4e91594dc1d664f94584bbdb8f21d6cdefc7b831
OmriLevyTau <omrilevy@mail.tau.ac.il>
2024-11-02 15:45:01 +0200

    Add SentenceTransformers encoders

--- nlp_course/encoder.py
+++ nlp_course/encoder.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from sentence_transformers import SentenceTransformer
 
 import torch
 from torch import nn
@@ -11,7 +12,7 @@ class BaseSentenceEncoder(ABC):
     represents this sentence.
     """
 
-    def __init__(self, model: nn.Module, model_name: str):
+    def __init__(self, model_name: str, model: nn.Module | None = None):
         self.model = model
         self.model_name = model_name
 
@@ -27,3 +28,31 @@ class LLM2VecEncoder(BaseSentenceEncoder):
 
     def encode(self, texts: list[str]) -> torch.Tensor:
         return self.model.encode(texts)
+
+
+class SentenceTransformersEncoder(BaseSentenceEncoder):
+
+    def __init__(self, model_name: str):
+        model: SentenceTransformer = SentenceTransformer(model_name)
+        super().__init__(model=model, model_name=model_name)
+
+    def encode(self, texts: list[str]) -> torch.Tensor:
+        embeddings = self.model.encode(texts)
+        return torch.from_numpy(embeddings)
+
+
+class PLMBERTBaserEncoder(BaseSentenceEncoder):
+    """
+    Stands for "Pre-trained Language model BERT based", which essentially mean
+    we're using an encoder which is a variation of BERT, and the assumption is that
+    it's regular encoder model, that is returns *vector for each token*.
+    """
+    pass
+
+
+
+if __name__ == "__main__":
+    sbert = SentenceTransformersEncoder(model_name="sentence-transformers/paraphrase-multilingual-mpnet-base-v2")
+    sentences = ["היי מה קורה?", "הכל טוב מלך?"]
+    embeddings =sbert.encode(sentences)
+    print(embeddings)
