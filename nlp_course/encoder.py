3283ff0087308729d08f58206893afd373041b15
OmriLevyTau <omrilevy@mail.tau.ac.il>
2024-11-02 20:45:37 +0200

    Add lazy encoder initialization That is - do not load model into memory before the last moment we need to generate text (good for caching)

--- nlp_course/encoder.py
+++ nlp_course/encoder.py
@@ -1,8 +1,12 @@
 from abc import ABC, abstractmethod
-from sentence_transformers import SentenceTransformer
+from pathlib import Path
 
 import torch
-from torch import nn
+from sentence_transformers import SentenceTransformer
+
+from llm2vec import LLM2Vec
+from nlp_course import BASE_DIR
+from nlp_course.utils import get_device
 
 
 class BaseSentenceEncoder(ABC):
@@ -12,8 +16,7 @@ class BaseSentenceEncoder(ABC):
     represents this sentence.
     """
 
-    def __init__(self, model_name: str, model: nn.Module | None = None):
-        self.model = model
+    def __init__(self, model_name: str):
         self.model_name = model_name
 
     @abstractmethod
@@ -25,19 +28,49 @@ class BaseSentenceEncoder(ABC):
 
 
 class LLM2VecEncoder(BaseSentenceEncoder):
+    default_base_model = "dicta-il/dictalm2.0-instruct"
+    default_peft_model = (
+        BASE_DIR / "output" / "mntp-simcse" / "dictalm2.0-instruct" / "checkpoint-1000"
+    )
+
+    def __init__(
+        self,
+        model_name: str,
+        base_model_name_or_path: str | Path | None = None,
+        peft_model_name_or_path: str | Path | None = None,
+    ):
+        super().__init__(model_name)
+        self.base_model = base_model_name_or_path or self.default_base_model
+        self.peft_model = peft_model_name_or_path or self.default_peft_model
+        self.model = None
+
+    def _initialize(self) -> None:
+        if self.model is None:
+            self.model = LLM2Vec.from_pretrained(
+                base_model_name_or_path=self.base_model,
+                peft_model_name_or_path=self.peft_model,
+                device_map=get_device(),
+                torch_dtype=torch.bfloat16,
+            )
 
     def encode(self, texts: list[str]) -> torch.Tensor:
+        self._initialize()  # apply lazy loading
         return self.model.encode(texts)
 
 
 class SentenceTransformersEncoder(BaseSentenceEncoder):
 
     def __init__(self, model_name: str):
-        model: SentenceTransformer = SentenceTransformer(model_name)
-        super().__init__(model=model, model_name=model_name)
+        super().__init__(model_name=model_name)
+        self.model = None
+
+    def _initialize(self) -> None:
+        if self.model is None:
+            self.model: SentenceTransformer = SentenceTransformer(self.model_name)
 
     def encode(self, texts: list[str]) -> torch.Tensor:
-        embeddings = self.model.encode(texts)
+        self._initialize()  # apply lazy loading
+        embeddings = self.model.encode(texts, show_progress_bar=True)
         return torch.from_numpy(embeddings)
 
 
@@ -47,12 +80,14 @@ class PLMBERTBaserEncoder(BaseSentenceEncoder):
     we're using an encoder which is a variation of BERT, and the assumption is that
     it's regular encoder model, that is returns *vector for each token*.
     """
+
     pass
 
 
-
 if __name__ == "__main__":
-    sbert = SentenceTransformersEncoder(model_name="sentence-transformers/paraphrase-multilingual-mpnet-base-v2")
+    sbert = SentenceTransformersEncoder(
+        model_name="sentence-transformers/paraphrase-multilingual-mpnet-base-v2"
+    )
     sentences = ["היי מה קורה?", "הכל טוב מלך?"]
-    embeddings =sbert.encode(sentences)
+    embeddings = sbert.encode(sentences)
     print(embeddings)
